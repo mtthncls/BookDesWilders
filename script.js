14bbd106a6bbf77924862a930fd693072a220cad
Grégoire Suray <gregoire.suray@gmail.com>
2019-03-14 17:51:33 +0100

    creation table wilders

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("test fonctionnement js");
+
+const wilders = [
+    {
+        firstName = "Axel",
+        familyName = "CHANDELIER",
+        profilePic = "https://i.imgur.com/dsKXOVX.jpg",
+        fewWords =  "Développeur en formation, je suis courtois, travailleur et ponctuel. Je recherche un stage afin de compléter ma formation.",
+        languages =  "Javascript React Node.js HTML5.0 CSS3.0",
+        softSkills = "Travail d'équipe - Autonomie - Ponctualité",
+        mobility =  "Prêt à prendre le train tous les jours si nécessaire. Disponibilité jusque Paris.",
+        linkedinLink = "https://www.linkedin.com/in/axel-chandelier-6a6079181/",
+        githubLink = "https://github.com/Spartakyste",
+        emailLink = "axeletcana@hotmail.fr"
+    }
+
+    {
+        firstName = "Grégoire",
+        familyName = "SURAY",
+        profilePic = "https://i.imgur.com/5QztU6i.jpg",
+        fewWords =  "Avec un background de 5 ans en management et gestion, j'ai décidé de me réorienter vers le Web. Curieux et déterminé, je me donne toujours à 100% dans ce que j'entreprends.",
+        languages =  "Javascript React Node.js HTML5.0 CSS3.0",
+        softSkills = "Aime travailler en équipe, rigoureux et organisé.",
+        mobility =  "Reims - Bordeaux - Québec",
+        linkedinLink = "https://www.linkedin.com/in/gr%C3%A9goire-suray-38266a92/",
+        githubLink = "https://github.com/Dezedene",
+        emailLink = "gregoire.suray@gmail.com"
+    }
+
+]
