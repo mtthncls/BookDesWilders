d2d341feb2e607e1aa59be023833e78b18a04757
Matthieu NICOLAS <mtthncls@gmail.com>
2019-03-15 11:12:59 +0100

    teachers + wilders cards + modal JS ok

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const wilders = [
 
 ]
 
-const leaders = [
+const teachers = [
     {
         firstName : "Thomas",
         familyName : "CULDAUT",
@@ -95,7 +95,7 @@ const leaders = [
         profilePic : "https://i.imgur.com/iFpWFRB.jpg",
         career :  "Après des fonctions marketing dans l’édition et les objets éducatifs, Clément s'est engagé dans l’enseignement supérieur au Vietnam puis en France (insertion professionnelle, développement personnel et recrutement innovant). Basé à Reims depuis quelques années, il s’implique dans les initiatives locales, notamment l’organisation de l'édition rémoise de TEDx. Passionné par les nouvelles approches pédagogiques, il a rejoint la Wild Code School comme Campus Manager en janvier 2018.",
         about :  "Clément à pour passion d'aller à la découverte des cultures du monde, et surtout de tout ce qui se mange !",
-        linkedinLink : "https://www.linkedin.com/in/etienne-rabin/",
+        linkedinLink : "https://www.linkedin.com/in/clementbechetoille/",
         emailLink : "clement@wildcodeschool.fr"
     }
 ]
@@ -107,10 +107,69 @@ console.log(wilders[2])
 const wildersElem = document.getElementById('wilders')
 console.log(wildersElem)
 //Define loop for wilders cards
-let card = ""
+let cardWilders = ""
 for (i = 0; i < wilders.length; i++){
     console.log(`${wilders[i].firstName} ${wilders[i].familyName} ${wilders[i].profilePic}`);
-    card += ` <article class="col-12 col-md-4"><div>
+    cardWilders += ` <article class="col-12 col-md-4"><div class="modal fade" id="${wilders[i].firstName}${wilders[i].familyName}Modal" tabindex="-1" role="dialog"
+    aria-labelledby="${wilders[i].firstName}${wilders[i].familyName}ModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center w-100" id="${wilders[i].firstName}${wilders[i].familyName}ModalLabel">${wilders[i].firstName} ${wilders[i].familyName}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modale">
+                <div class="modal-body row justify-content-center align-items-center">
+                    <div class="col-12 col-md-6">
+                        <img class="w-100 rounded-circle modimg"
+                            src="${wilders[i].profilePic}" alt="photo ${wilders[i].firstName} ${wilders[i].familyName}">
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <p class="souligne">Quelques mots de notre wilder</p>
+                        <p class="text-justify">${wilders[i].fewWords}</p>
+                        <div>
+                            <p class="souligne">Langages de programmation</p>
+                            <p>${wilders[i].languages}</p>
+                        </div>
+                        <div>
+                            <p class="souligne">Soft Skills</p>
+                            <p>${wilders[i].softSkills}</p>
+                        </div>
+                        <div>
+                            <p class="souligne">Mobilité professionnelle</p>
+                            <p>${wilders[i].mobility}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="modal-footer justify-content-center">
+                    <div class="col imagebox">
+                        <a href="${wilders[i].linkedinLink}"
+                            target="_blank">
+                            <img class="img-fluid rounded-circle"
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW0m7yXW45tMa1MmBfOm904CyHUrPYTK8BZufKdA5O4NtvvSui"
+                                alt="Responsive image"></a>
+                    </div>
+                    <div class="col imagebox">
+                        <a href="${wilders[i].githubLink}" target="_blank">
+                            <img class="img-fluid rounded-circle"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png"
+                                alt="Responsive image"></a>
+                    </div>
+                    <div class="col imagebox">
+                        <a href="${wilders[i].emailLink}" target="_blank">
+                            <img class="img-fluid"
+                                src="http://www.cril54.org/wp-content/uploads/2018/04/enveloppe-mail.png"
+                                alt="Responsive image"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><div>
             <img src="${wilders[i].profilePic}" class="card-img-top rounded-circle test modimg"
                 alt="photo ${wilders[i].firstName} ${wilders[i].familyName}">
         </div>
@@ -120,5 +179,76 @@ for (i = 0; i < wilders.length; i++){
                 profil</a>
         </div></article>` 
     }
-console.log("card", card)
-wildersElem.innerHTML = card;
+console.log("card", cardWilders)
+wildersElem.innerHTML = cardWilders;
+
+//Get the element with teachers id
+const teachersElem = document.getElementById('teachers')
+console.log(teachersElem)
+//Define loop for teachers cards
+let cardTeachers = ""
+for (i = 0; i < teachers.length; i++){
+    console.log(`${teachers[i].firstName} ${teachers[i].familyName} ${teachers[i].profilePic}`);
+    cardTeachers += `<article class="col-12 col-md-4">
+    <div class="modal fade" id="${teachers[i].firstName}${teachers[i].familyName}Modal" tabindex="-1" role="dialog"
+        aria-labelledby="${teachers[i].firstName}${teachers[i].familyName}ModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-center w-100" id="${teachers[i].firstName}${teachers[i].familyName}ModalLabel">${teachers[i].firstName} ${teachers[i].familyName}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modale">
+                    <div class="modal-body row justify-content-center align-items-center">
+                        <div class="col-12 col-md-6">
+                            <img class="w-100 rounded-circle modimg"
+                                src="${teachers[i].profilePic}" alt="photo ${teachers[i].firstName} ${teachers[i].familyName}">
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <div>
+                                <p class="souligne">Son parcours</p>
+                                <p>${teachers[i].career}</p>
+                            </div>
+                            <div>
+                                <p class="souligne">Une anecdote</p>
+                                <p>${teachers[i].about}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="modal-footer justify-content-center">
+                        <div class="col imagebox">
+                            <a href="${teachers[i].linkedinLink}"
+                                target="_blank">
+                                <img class="img-fluid rounded-circle"
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW0m7yXW45tMa1MmBfOm904CyHUrPYTK8BZufKdA5O4NtvvSui"
+                                    alt="Responsive image"></a>
+                        </div>
+                        <div class="col imagebox">
+                            <a href="${teachers[i].emailLink}" target="_blank">
+                                <img class="img-fluid"
+                                    src="http://www.cril54.org/wp-content/uploads/2018/04/enveloppe-mail.png"
+                                    alt="Responsive image"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <img src="${teachers[i].profilePic}" class="card-img-top rounded-circle test modimg"
+            alt="${teachers[i].firstName} ${teachers[i].familyName}">
+    </div>
+    <div class="card-body border-0">
+        <h5 class="card-title font-weight-bold">${teachers[i].firstName} <br />${teachers[i].familyName}</h5>
+        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#${teachers[i].firstName}${teachers[i].familyName}Modal">Voir le
+            profil</a>
+    </div>
+</article>` 
+    }
+console.log("card", cardTeachers)
+teachersElem.innerHTML = cardTeachers;
