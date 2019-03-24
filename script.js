ddafb83503b4fa221e4bf484d6d3199aa5f08916
Matthieu <45939940+mtthncls@users.noreply.github.com>
2019-03-24 21:26:18 +0100

    Final

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ const teachers = [{
     {
         firstName: "Etienne",
         familyName: "RABIN",
-        profilePic: "https://i.imgur.com/DdN2Su4.png",
+        profilePic: "https://i.imgur.com/9EfD08d.jpg",
         career: "Sa passion pour le dessin l'a naturellement amené à faire des études de graphisme. Mais sa nature curieuse et sa volonté d'élargir ses champs de création l'ont amené à rejoindre la WildCodeSchool, d'abord comme élève puis aujourd'hui comme formateur.",
         about: "Ne prononcez pas les mots \"PSG\" et \"Remontada\" devant lui !",
         linkedinLink: "https://www.linkedin.com/in/etienne-rabin/",
@@ -88,7 +88,7 @@ const teachers = [{
     {
         firstName: "Clément",
         familyName: "BECHETOILLE",
-        profilePic: "https://i.imgur.com/erXJsNE.png",
+        profilePic: "https://i.imgur.com/skIwyZh.jpg",
         career: "Après des fonctions marketing dans l’édition et les objets éducatifs, Clément s'est engagé dans l’enseignement supérieur au Vietnam puis en France (insertion professionnelle, développement personnel et recrutement innovant). Basé à Reims depuis quelques années, il s’implique dans les initiatives locales, notamment l’organisation de l'édition rémoise de TEDx. Passionné par les nouvelles approches pédagogiques, il a rejoint la Wild Code School comme Campus Manager en janvier 2018.",
         about: "Clément à pour passion d'aller à la découverte des cultures du monde, et surtout de tout ce qui se mange !",
         linkedinLink: "https://www.linkedin.com/in/clementbechetoille/",
